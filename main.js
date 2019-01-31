acd28f4492fd14198e0f89574e0c4ea341b402c5
eamobi <amobisomto@gmail.com>
2019-01-31 15:30:31 -0600

    small formatting

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 //Get all the keys from the body
 var keys = document.getElementsByTagName('td');
-var funcs = ['+','-','x','/'];
-function showVal(e){
+//will use this array later. 
+var funcs = ['+', '-', 'x', '/'];
+function showVal(e) {
     //get the input values
     var input = document.querySelector('.screen');
     var inputValue = input.innerHTML;
     var btnValue = this.innerHTML;
     //Get the key values and append it to the input string, and evaluate it.
     //Set the input to empty if clear is pressed, to.
-    if(btnValue === 'C'){
+    if (btnValue === 'C') {
         input.innerHTML = '';
     }
-    //Use regex and replace 'x' with '*', so it evaluates properly 
-    eqn = eqn.replace(/x/g,'*');
     //Calculate and display the solution if '=' is pressed
-    else if(btnValue === '='){
+    else if (btnValue === '=') {
         let eqn = inputValue;
+        //Use regex and replace 'x' with '*', so it evaluates properly 
+        eqn = eqn.replace(/x/g, '*');
         input.innerHTML = eval(eqn);
 
     }
     //If any key is pressed append it 
-    else{
+    else {
         input.innerHTML += btnValue;
     }
 }
 //Add onclick events
-for (let i = 0; i < keys.length; i++){
-    keys[i].addEventListener('click',showVal)
+for (let i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('click', showVal)
 }
 
 
